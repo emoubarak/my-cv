45a37547eee92a939ee092fd92b2e23652c72257
El Mahdi Moubarak <moubarakelmahdi1@gmail.com>
2026-03-25 02:02:57 +0100

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://emoubarak.github.io',
+  base: '/my-cv',
   vite: {
     plugins: [tailwindcss()]
   }
